fefe88aeacd7dd3664fec1990259233863dcc4a4
Lakshya <lakshya.anand@dxc.com>
2024-08-30 18:46:43 +0530

    Updated name of Login.tsx

--- ShadCN_graph/src/App.tsx
+++ ShadCN_graph/src/App.tsx
@@ -1,4 +1,4 @@
-import Login from './components/Login.';
+import Login from './components/Login';
 import MainLayout from './components/MainLayout';
 import Dashboard2 from './components/Dashboard2';
 import VoiceTest from './components/VoiceTest';
